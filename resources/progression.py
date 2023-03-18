b0adcf9bd830b6de80ff5733e3eab0db207b222b
Daniel <danieldev1999@gmail.com>
2023-03-18 20:26:50 -0300

    :wrench: Add Progression class

---
+++ resources/progression.py
@@ -0,0 +1,43 @@
+class Progression:
+    """Iterator producing a generic progression.
+
+    Default iterator produces the whole numbers: 0, 1, 2, ...
+    """
+
+    def __init__(self, start = 0):
+        """Initialize current to the first value of the progression."""
+        self._current = start
+
+    def _advance(self):
+        """Update self._current to a new value.
+        
+        This should be overriden by a subclass to customize progression.
+
+        By convention, if current is set to None, this designates the
+        end of a finite progression.
+        """
+        self._current += 1
+
+    def __next__(self):
+        """Return the next element, or else raise StopIteration error."""
+        if self._current is None:           # our convention to end a progression
+            raise StopIteration()
+        else:
+            answer = self._current          # record current value to return
+            self._advance()                 # advance to prepare for next time
+            return answer
+
+    def __iter__(self):
+        """By convention, an iterator must return itself as an iterator. """
+        return self
+    
+    def print_progression(self, n):
+        values = []
+        for j in range(n):
+            values.append(next(self))
+        return values
+
+if __name__ == '__main__':
+    progression = Progression(2)            # construct progression
+    val = progression.print_progression(3)
+    print(val)
