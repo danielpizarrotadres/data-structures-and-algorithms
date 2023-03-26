685c13013bdc7b83d24b1a05caf1251b4aedf259
Daniel <danieldev1999@gmail.com>
2023-03-26 17:59:39 -0300

    :construction: Wip WaveSort class

---
+++ resources/sort/wave_sort.py
@@ -0,0 +1,10 @@
+class WaveSort:
+    """Sorter that receive a progression of numbers
+
+    Default sort produces the whole numbers: 2, 1, 3, ...
+    """
+
+    def __init__(self, numbers):
+        """Initialize numbers to the given numbers."""
+        self._numbers = numbers
+        
