e30a0f6e1b2d105965cd18c532de02199d77f429
Daniel <danieldev1999@gmail.com>
2023-03-26 18:48:05 -0300

    :construction: Build WaveSort class

--- resources/sort/wave_sort.py
+++ resources/sort/wave_sort.py
@@ -7,4 +7,20 @@ class WaveSort:
     def __init__(self, numbers):
         """Initialize numbers to the given numbers."""
         self._numbers = numbers
-        
+    
+    def search(self, currentPos, nextPos):
+        currentPosValue, result = self._numbers[currentPos]
+        nextPosValue = self._numbers[nextPos]
+        if currentPosValue >= nextPosValue:
+            return result
+
+    def sort(self):
+        print(len(self._numbers))
+        for j in range(len(self._numbers)):
+            print(j)
+            numbers[j] = search(j, j + 1)
+        print(numbers)
+
+if __name__ == '__main__':
+    data = WaveSort([0, 1, 2, 3])
+    data.sort()
