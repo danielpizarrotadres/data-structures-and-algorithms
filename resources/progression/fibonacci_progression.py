9fcb88562bc9440ce3de4e5696e952c3f4f66864
Daniel <118082275+danielpizarrotadres@users.noreply.github.com>
2023-04-08 18:26:42 -0400

    :sparkles: Add FibonacciProgression

--- resources/progression/fibonacci_progression.py
+++ resources/progression/fibonacci_progression.py
@@ -1,1 +1,22 @@
+from progression import Progression
 
+class FibonacciProgression(Progression):       # inherit from Progression
+    """Iterator producing a Fibonacci progression."""
+
+    def __init__(self, first = 0, second = 1):
+        """Create a new fibonacci progression.
+
+        first        the first term of the progression  (default 0)
+        second       the second term of the progression (default 1)
+        """
+        super().__init__(first)                # initialize base class
+        self._prev = second - first.           # fictitious value preceding the first
+
+    def _advance(self):                        # override inherited version
+        """Update current value by adding the fixed increment."""
+        self._prev, self._current = self._current, self._prev + self._current
+
+if __name__ == '__main__':
+    print('Default progression:')
+    default_progression = Progression().print_progression(10)
+    print(default_progression)
