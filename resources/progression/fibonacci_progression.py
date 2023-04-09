1906f0539089253ed5b0fdd278b91147c5a210ce
Daniel <danieldev1999@gmail.com>
2023-04-09 14:37:36 -0400

    :white_check_mark: :adhesive_bandage: Add test for FibonacciProgression class and remove wrong point

--- resources/progression/fibonacci_progression.py
+++ resources/progression/fibonacci_progression.py
@@ -10,13 +10,18 @@ class FibonacciProgression(Progression):       # inherit from Progression
         second       the second term of the progression (default 1)
         """
         super().__init__(first)                # initialize base class
-        self._prev = second - first.           # fictitious value preceding the first
+        self._prev = second - first            # fictitious value preceding the first
 
     def _advance(self):                        # override inherited version
         """Update current value by adding the fixed increment."""
         self._prev, self._current = self._current, self._prev + self._current
 
 if __name__ == '__main__':
-    print('Default progression:')
-    default_progression = Progression().print_progression(10)
-    print(default_progression)
+    # print('Default progression:')
+    # default_progression = Progression().print_progression(10)
+    # print(default_progression)
+
+    print('FibonacciProgression')
+    fibo = FibonacciProgression(4, 6).print_progression(3)
+    print('------- end')
+    print(fibo)
